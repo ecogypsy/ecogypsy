80924d77701ea9f41af7a29d11cab9ebcfbfa203
ravisingh2 <raviducat@gmail.com>
2017-10-31 22:37:12 +0530

    minor chantes

--- module/Admin/config/constant.php
+++ module/Admin/config/constant.php
@@ -1,6 +1,6 @@
 <?php
 $GLOBALS['SITE_ADMIN_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/ecogypsy/admin/';
-$GLOBALS['SITE_APP_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/ecogypsy/application/index';
+$GLOBALS['SITE_APP_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/ecogypsy/application/';
 $GLOBALS['PAGE_BEFORE_LOGIN'] = array('Admin\Controller\Index\login','Admin\Controller\Index\index');
 $GLOBALS['SITE_PATH'] = $_SERVER['DOCUMENT_ROOT'];
 $GLOBALS['HOTELIMAGEPATH'] = $_SERVER['DOCUMENT_ROOT'].'ecogypsy/hotel';
